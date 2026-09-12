22c65983707c56b497b644a8cf45470da0969a00
Youssef Tammam <youssef2016tammam@gmail.com>
2026-09-12 20:00:41 +0300

    Restructuring folders and files

---
+++ state.py
@@ -0,0 +1,28 @@
+import operator
+from typing import TypedDict, Annotated, Sequence, List, Optional
+from langchain_core.messages import BaseMessage
+from pydantic import BaseModel, Field
+
+class ResearchGraphState(TypedDict):
+    """The shared memory clipboard passed between all agents in the graph.
+    Sho8l fa5er men el 2a5er
+    """
+    # Tracks the conversation history. operator.add ensures we append new messages, not overwrite them.
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+    # The routing variable the Supervisor uses to direct traffic
+    next_agent: str
+
+    #Isolated state variable to store data without clogging the main LLM context window.
+    extracted_data: List[str]
+    verification_status: Optional[str]
+
+class SupervisorDecision(BaseModel):
+    """The Pydantic guardrail that forces the LLM to output valid JSON routing."""
+
+    next_agent: str = Field(
+        description = "The next agent to route to. MUST be one of: 'Researcher', Verifier', 'Memory', 'FINISH'."
+    )
+    instructions: str = Field(
+        description = "Highly specific instructions telling the selected agent exactly what to do next based on the current state."
+    )
